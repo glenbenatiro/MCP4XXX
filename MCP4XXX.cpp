c0f6484f54eac7cdfd08290e1ae1516ecd40ba49
Glen <glenbenatiro@gmail.com>
2023-04-29 02:13:11 +0800

    MCP4XXX v0.1 Build 1

--- MCP4XXX.cpp
+++ MCP4XXX.cpp
@@ -7,14 +7,8 @@ MCP4XXX::
 MCP4XXX (MCP4XXX::PART_NUMBER part, 
          MCP4XXX::RESISTANCE_VERSION resistance)
 {
-  resolution        (part);
   total_resistance  (resistance);
-}
-
-MCP4XXX::
-~MCP4XXX ()
-{
-
+  resolution        (part);
 }
 
 void MCP4XXX::
@@ -28,8 +22,6 @@ resolution (MCP4XXX::PART_NUMBER part)
     case MCP4XXX::PART_NUMBER::MCP4242:
     {
       m_resolution_bits = 7;
-      m_resolution_int  = 128;
-
       break;
     }
 
@@ -39,11 +31,12 @@ resolution (MCP4XXX::PART_NUMBER part)
     case MCP4XXX::PART_NUMBER::MCP4262:
     {
       m_resolution_bits = 8;
-      m_resolution_int  = 256;
-
       break;
     }
   }
+
+  m_resolution_int  = std::pow (2, m_resolution_bits);
+  m_step_resistance = m_max_resistance / m_resolution_int;
 }
 
 void MCP4XXX::
@@ -52,76 +45,36 @@ total_resistance (MCP4XXX::RESISTANCE_VERSION resistance)
   m_max_resistance = static_cast<double>(resistance) + WIPER_RESISTANCE;
 }
 
-double MCP4XXX::
+void MCP4XXX:: 
 theoretical_resistance (unsigned wiper_value)
 {
   double val = ((m_max_resistance * (wiper_value)) / m_resolution_int) + 
     WIPER_RESISTANCE;
-  
-  return (val);
+
+  m_theoretical_resistance = val;
 }
 
 void MCP4XXX::
-write_data (MCP4XXX::MEM_ADDR addr, 
+write_data (uint8_t          addr, 
             MCP4XXX::COMMAND cmd)
 {
-  char txdata = (static_cast<uint8_t>(addr) & 0xf) << 4 | 
-                (static_cast<uint8_t>(cmd) & 0x3) << 2;
+  char txdata = (addr & 0xf) << 4 | (static_cast<uint8_t>(cmd) & 0x3) << 2;
                         
-  spi_write (&txdata, 1);
+  spi_xfer (&txdata, 1);
 }
 
 void MCP4XXX:: 
-write_data (MCP4XXX::MEM_ADDR addr,
+write_data (uint8_t           addr,
             MCP4XXX::COMMAND  cmd, 
             uint16_t          data)
 {
-  uint8_t temp      = (static_cast<uint8_t>(addr) & 0xf) << 12 | 
+  uint8_t temp      = (addr & 0xf) << 12 | 
                       (static_cast<uint8_t>(cmd)  & 0x3) << 10 | 
                       (data & 0x3ff);
                         
   char    txdata[2] = {temp & 0xff, (temp & 0xff00) >> 8};
 
-  spi_write (txdata, 2);
-}
-
-void MCP4XXX::
-increment  (unsigned steps)
-{
-
-}
-
-void MCP4XXX::
-decrement  (unsigned steps)
-{
-
-}
-
-double MCP4XXX::
-resistance (bool   channel,
-            double value)
-{
-  if (is_valid_resistance (value))
-  {
-    double n = std::round ((m_resolution_int * 
-      (value - WIPER_RESISTANCE)) / m_max_resistance);
-
-    m_wiper_value = static_cast<uint16_t>(n);
-
-    write_data (
-      channel ? MEM_ADDR::V_WIPER_1 : MEM_ADDR::V_WIPER_0,
-      COMMAND::WRITE,
-      m_wiper_value
-    );
-
-    m_theoretical_resistance = theoretical_resistance (m_wiper_value);
-  }
-  else 
-  {
-    throw (std::out_of_range ("Resistance value invalid."));
-  }
-
-  return (m_theoretical_resistance);
+  spi_xfer (txdata, 2);
 }
 
 bool MCP4XXX::
@@ -136,4 +89,86 @@ is_valid_resistance (double value)
     return (false);
   }
 }
+
+uint16_t MCP4XXX:: 
+hw_wiper_value ()
+{
+
+}
+
+void MCP4XXX::
+increment (bool     channel,
+           unsigned steps)
+{
+  for (int i = 0; i < steps; i++)
+  {
+    if (m_wiper_value >= m_resolution_int)
+    {
+      break;
+    }
+    else 
+    {
+      write_data (
+        static_cast<uint8_t>(channel ? WIPER_ADDR::VOLATILE_WIPER_1 : 
+          WIPER_ADDR::VOLATILE_WIPER_0),
+        COMMAND::INCREMENT
+      );
+
+      theoretical_resistance (++m_wiper_value);
+    }
+  }
+}
+
+void MCP4XXX::
+decrement  (bool     channel,
+            unsigned steps)
+{
+  for (int i = 0; i < steps; i++)
+  {
+    if (m_wiper_value <= 0)
+    {
+      break;
+    }
+    else 
+    {
+      write_data (
+        static_cast<uint8_t>(channel ? WIPER_ADDR::VOLATILE_WIPER_1 : 
+          WIPER_ADDR::VOLATILE_WIPER_0),
+        COMMAND::DECREMENT
+      );
+
+      theoretical_resistance (--m_wiper_value);
+    }
+  }
+}
+
+double MCP4XXX::
+resistance (bool   channel,
+            double value)
+{
+  if (is_valid_resistance (value))
+  {
+    double n = std::round ((m_resolution_int * 
+      (value - WIPER_RESISTANCE)) / m_max_resistance);
+
+    m_wiper_value = static_cast<uint16_t>(n);
+
+    write_data (
+      static_cast<uint8_t>(channel ? WIPER_ADDR::VOLATILE_WIPER_1 : 
+        WIPER_ADDR::VOLATILE_WIPER_0),
+      COMMAND::WRITE,
+      m_wiper_value
+    );
+
+    theoretical_resistance (m_wiper_value);
+  }
+  else 
+  {
+    throw (std::out_of_range ("Resistance value invalid."));
+  }
+
+  return (m_theoretical_resistance);
+}
+
+// eof
 
--- MCP4XXX.h
+++ MCP4XXX.h
@@ -6,27 +6,47 @@
 class MCP4XXX
 {
   // https://ww1.microchip.com/downloads/en/DeviceDoc/22059b.pdf
-  public:
-    enum class MEM_ADDR
+
+  private:
+    enum class WIPER_ADDR
     {
-      V_WIPER_0   = 0x00,
-      V_WIPER_1   = 0x01,
-      NV_WIPER_0  = 0x02,
-      NV_WIPER_1  = 0X03,
-      TCON_REG    = 0x04,
-      STAT_REG    = 0x05,
-      DATA_0      = 0x06,
-      DATA_1      = 0x07,
-      DATA_2      = 0x08,
-      DATA_3      = 0x09,
-      DATA_4      = 0x0a,
-      DATA_5      = 0x0b,
-      DATA_6      = 0x0c,
-      DATA_7      = 0x0d,
-      DATA_8      = 0x0e,
-      DATA_9      = 0x0f
+      VOLATILE_WIPER_0      = 0x00,
+      VOLATILE_WIPER_1      = 0x01,
+      NON_VOLATILE_WIPER_0  = 0x02,
+      NON_VOLATILE_WIPER_1  = 0x03
     };
 
+    enum class REG_ADDR
+    {
+      TCON    = 0x04,
+      STATUS  = 0x05
+    };
+
+    enum class DATA_ADDR  
+    {
+      DATA_0 = 0x06,
+      DATA_1 = 0x07,
+      DATA_2 = 0x08,
+      DATA_3 = 0x09,
+      DATA_4 = 0x0a,
+      DATA_5 = 0x0b,
+      DATA_6 = 0x0c,
+      DATA_7 = 0x0d,
+      DATA_8 = 0x0e,
+      DATA_9 = 0x0f
+    };
+
+    enum class COMMAND
+    {
+      WRITE     = 0,
+      INCREMENT = 1,
+      DECREMENT = 2,
+      READ      = 3
+    };
+
+    static constexpr double WIPER_RESISTANCE  = 75; 
+
+  public:
     enum class PART_NUMBER
     {
       MCP4141,
@@ -47,43 +67,34 @@ class MCP4XXX
       _104  = 100'000
     };
 
-    enum class COMMAND
-    {
-      WRITE     = 0,
-      INCREMENT = 1,
-      DECREMENT = 2,
-      READ      = 3
-    };
-
-    static constexpr double WIPER_RESISTANCE  = 75; // ideal resistance
-
   private: 
-    void*     m_controller;
-    uint16_t  m_wiper_value;
-    unsigned  m_resolution_int;
-    unsigned  m_resolution_bits;
-    double    m_max_resistance;
-    double    m_step_resistance;
-    double    m_theoretical_resistance;
-    double    m_min_resistance = WIPER_RESISTANCE;
+    void*     m_controller              = nullptr;
+    uint16_t  m_wiper_value             = 0x80;
+    unsigned  m_resolution_int          = 8,
+              m_resolution_bits         = 256;
+    double    m_max_resistance          = 50'000.0,
+              m_step_resistance         = 195.3125,
+              m_theoretical_resistance  = 25'000.0 + WIPER_RESISTANCE,
+              m_min_resistance          = WIPER_RESISTANCE;
     
-    void      resolution              (MCP4XXX::PART_NUMBER part);
-    void      total_resistance        (MCP4XXX::RESISTANCE_VERSION resistance);
-    double    theoretical_resistance  (unsigned wiper_value);
+    void      resolution                  (MCP4XXX::PART_NUMBER part);
+    void      total_resistance            (MCP4XXX::RESISTANCE_VERSION resistance);
+    void      theoretical_resistance      (unsigned wiper_value);
+    void      write_data                  (uint8_t addr, MCP4XXX::COMMAND cmd);
+    void      write_data                  (uint8_t addr, MCP4XXX::COMMAND cmd, uint16_t data);
+    bool      is_valid_resistance         (double value);
+    uint16_t  hw_wiper_value              ();
+  
+  protected:
+    virtual void init     (void* controller)            = 0;
+    virtual void spi_xfer (char* data, unsigned length) = 0;
 
   public:
     MCP4XXX   (MCP4XXX::PART_NUMBER part, MCP4XXX::RESISTANCE_VERSION resistance);
-    ~MCP4XXX  ();
-
-    void    write_data          (MCP4XXX::MEM_ADDR addr, MCP4XXX::COMMAND cmd);
-    void    write_data          (MCP4XXX::MEM_ADDR addr, MCP4XXX::COMMAND cmd, uint16_t data);
-    void    increment           (unsigned steps = 1);
-    void    decrement           (unsigned steps = 1);
-    double  resistance          (bool channel, double value);
-    bool    is_valid_resistance (double value);
-
-    virtual void init           (void* controller)            = 0;
-    virtual void spi_write      (char* data, unsigned length) = 0;
+    
+    void    increment   (bool channel, unsigned steps = 1);
+    void    decrement   (bool channel, unsigned steps = 1);
+    double  resistance  (bool channel, double value);
 };
 
 #endif
